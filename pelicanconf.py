dd334703179e894207f57b8aec93546f8cbfae1f
Alberto Donato <alberto.donato@gmail.com>
2014-10-17 21:09:11 +0200

    WIP.

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+
 from __future__ import unicode_literals
 
 AUTHOR = u'Alberto Donato'
@@ -8,26 +9,44 @@ SITEURL = ''
 
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'UTC'
 
 DEFAULT_LANG = u'en'
+DEFAULT_DATE_FORMAT = '%Y/%m/%d'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = False
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = ()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('Launchpad', 'https://launchpad.net/~ack'),
+    ('BitBucket', 'https://bitbucket.org/ack'))
 
 DEFAULT_PAGINATION = 10
 
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = False
+
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
+
+THEME = '../pelican-bootstrap3'
+
+# pelican-bootstrap3 settings
+
+BOOTSTRAP_THEME = 'spacelab'  # united cerulean slate spacelab
+
+SHOW_ARTICLE_AUTHOR = True
+SHOW_ARTICLE_CATEGORY = True
+
+PYGMENTS_STYLE = 'default'
+# PYGMENTS_RST_OPTIONS = {'linenos': 'inline'}
+
+# CUSTOM_CSS = 'static/custom.css'
+
+ABOUT_ME = "Alberto Donato"
