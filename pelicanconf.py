32946855a16a7415652a82e1b661689bd8c55981
Alberto Donato <alberto.donato@gmail.com>
2015-06-07 15:43:38 +0300

    Cleanups.

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,5 +1,5 @@
 AUTHOR = 'Alberto Donato'
-SITENAME = 'Significant white space'
+SITENAME = "Ack's blog"
 SITEURL = 'http://localhost:8000'
 
 RELATIVE_URLS = True
@@ -20,21 +20,19 @@ CATEGORY_URL = 'category/{slug}'
 CATEGORY_SAVE_AS = 'category/{slug}.html'
 YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
 MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
-
 CATEGORY_SAVE_URL = 'category/{slug}.html'
+
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
+# Links section
 LINKS = ()
 
-# Social widget
+# Social links
 SOCIAL = (
-    ('launchpad', 'https://launchpad.net/~ack'),
     ('bitbucket', 'https://bitbucket.org/ack'),
-    ('google+', 'https://plus.google.com/104904437381999058439'),
-    ('linkedin', 'https://it.linkedin.com/in/albertodonato'),
+    ('launchpad', 'https://launchpad.net/~ack'),
     ('rss', SITEURL + '/' + FEED_ALL_ATOM))
 
 DEFAULT_PAGINATION = 5
@@ -44,11 +42,6 @@ DEFAULT_CATEGORY = 'misc'
 TAG_CLOUD_MAX_ITEMS = 10
 
 DISPLAY_CATEGORIES_ON_MENU = False
-DISPLAY_CATEGORIES_ON_SIDEBAR = True
-DISPLAY_PAGES_ON_MENU = True
-DISPLAY_TAGS_ON_SIDEBAR = True
-
-CC_LICENSE = "CC-BY-NC-SA"
 
 STATIC_PATHS = [
     'images', 'files', 'extra/robots.txt', 'extra/custom.css']
@@ -58,8 +51,7 @@ EXTRA_PATH_METADATA = {
     'extra/custom.css': {'path': 'static/custom.css'},
     'extra/favicon.ico': {'path': 'static/favicon.ico'}}
 
-
-THEME = '../pelican-bootstrap3'
+THEME = '../pelican-themes/pelican-bootstrap3'
 
 PLUGIN_PATHS = ['../pelican-plugins']
 
@@ -68,16 +60,25 @@ PLUGIN_PATHS = ['../pelican-plugins']
 #            'liquid_tags.include_code', 'liquid_tags.notebook']
 
 
+PYGMENTS_STYLE = 'default'
+# PYGMENTS_RST_OPTIONS = {'linenos': 'inline'}
+
+#
+# pelican-bootstrap3 theme-specific settings
+#
+
 BOOTSTRAP_THEME = 'cerulean'
 
 BOOTSTRAP_NAVBAR_INVERSE = True
 
+DISPLAY_CATEGORIES_ON_SIDEBAR = True
+DISPLAY_TAGS_ON_SIDEBAR = True
+
 SHOW_ARTICLE_AUTHOR = False
 SHOW_ARTICLE_CATEGORY = True
 
-DISPLAY_BREADCRUMBS = True
+DISPLAY_BREADCRUMBS = False
 
-PYGMENTS_STYLE = 'default'
-# PYGMENTS_RST_OPTIONS = {'linenos': 'inline'}
+CC_LICENSE = "CC-BY-NC-SA"
 
 CUSTOM_CSS = 'static/custom.css'
