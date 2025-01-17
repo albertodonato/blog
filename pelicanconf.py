db99581eb993b26a2231a1fef5546e28b61d48f0
Alberto Donato <alberto.donato@gmail.com>
2025-01-17 12:09:11 +0100

    move to tox

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,65 +1,63 @@
 import pelican
 
-AUTHOR = 'Alberto Donato'
-SITENAME = 'Significant white space'
-SITEURL = 'http://localhost:8000'
+AUTHOR = "Alberto Donato"
+SITENAME = "Significant white space"
+SITEURL = "http://localhost:8000"
 
 RELATIVE_URLS = True
 
-TIMEZONE = 'UTC'
+TIMEZONE = "UTC"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 DEFAULT_PAGINATION = 5
-DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+DEFAULT_DATE_FORMAT = "%Y-%m-%d"
 
 # URL formats
-ARTICLE_URL = 'posts/{slug}'
-ARTICLE_SAVE_AS = 'posts/{slug}.html'
-PAGE_URL = '{slug}'
-PAGE_SAVE_AS = '{slug}.html'
-TAGS_URL = 'tags'
-TAGS_SAVE_AS = 'tags.html'
-TAG_URL = 'tag/{slug}'
-TAG_SAVE_AS = 'tag/{slug}.html'
-CATEGORIES_URL = 'categories'
-CATEGORIES_SAVE_AS = 'categories.html'
-CATEGORY_URL = 'category/{slug}'
-CATEGORY_SAVE_AS = 'category/{slug}.html'
-ARCHIVES_URL = 'archives'
-ARCHIVES_SAVE_AS = 'archives/index.html'
-YEAR_ARCHIVE_URL = 'archives/{date:%Y}/index'
-YEAR_ARCHIVE_SAVE_AS = 'archives/{date:%Y}/index.html'
-MONTH_ARCHIVE_URL = 'archives/{date:%Y}/{date:%m}/index'
-MONTH_ARCHIVE_SAVE_AS = 'archives/{date:%Y}/{date:%m}/index.html'
-DAY_ARCHIVE_URL = 'archives/{date:%Y}/{date:%m}/{date:%d}/index'
-DAY_ARCHIVE_SAVE_AS = 'archives/{date:%Y}/{date:%m}/{date:%d}/index.html'
+ARTICLE_URL = "posts/{slug}"
+ARTICLE_SAVE_AS = "posts/{slug}.html"
+PAGE_URL = "{slug}"
+PAGE_SAVE_AS = "{slug}.html"
+TAGS_URL = "tags"
+TAGS_SAVE_AS = "tags.html"
+TAG_URL = "tag/{slug}"
+TAG_SAVE_AS = "tag/{slug}.html"
+CATEGORIES_URL = "categories"
+CATEGORIES_SAVE_AS = "categories.html"
+CATEGORY_URL = "category/{slug}"
+CATEGORY_SAVE_AS = "category/{slug}.html"
+ARCHIVES_URL = "archives"
+ARCHIVES_SAVE_AS = "archives/index.html"
+YEAR_ARCHIVE_URL = "archives/{date:%Y}/index"
+YEAR_ARCHIVE_SAVE_AS = "archives/{date:%Y}/index.html"
+MONTH_ARCHIVE_URL = "archives/{date:%Y}/{date:%m}/index"
+MONTH_ARCHIVE_SAVE_AS = "archives/{date:%Y}/{date:%m}/index.html"
+DAY_ARCHIVE_URL = "archives/{date:%Y}/{date:%m}/{date:%d}/index"
+DAY_ARCHIVE_SAVE_AS = "archives/{date:%Y}/{date:%m}/{date:%d}/index.html"
 
 # Links section
 LINKS = ()
 
 # Social links
 SOCIAL = (
-    ('github', 'https://github.com/albertodonato'),
-    ('linkedin', 'https://www.linkedin.com/in/albertodonato'),
-    ('mastodon', 'https://hackyderm.io/@ack'),
+    ("github", "https://github.com/albertodonato"),
+    ("linkedin", "https://www.linkedin.com/in/albertodonato"),
+    ("mastodon", "https://hackyderm.io/@ack"),
 )
 
-DIRECT_TEMPLATES = (
-    'index', 'categories', 'tags', 'authors', 'archives', 'search')
+DIRECT_TEMPLATES = ("index", "categories", "tags", "authors", "archives", "search")
 
 USE_FOLDER_AS_CATEGORY = False
-DEFAULT_CATEGORY = 'misc'
+DEFAULT_CATEGORY = "misc"
 
-STATIC_PATHS = ['images', 'files', 'extra/robots.txt']
+STATIC_PATHS = ["images", "files", "extra/robots.txt"]
 
-EXTRA_PATH_METADATA = {
-    'extra/robots.txt': {'path': 'robots.txt'}}
+EXTRA_PATH_METADATA = {"extra/robots.txt": {"path": "robots.txt"}}
 
-THEME = '../pelican-chemistry'
+THEME = "../pelican-chemistry"
 
-PLUGIN_PATHS = ['../pelican-plugins']
+PLUGIN_PATHS = ["../pelican-plugins"]
 
-PLUGINS = ['headerid.headerid', 'tipue_search', 'sitemap']
+PLUGINS = ["sitemap"]
 
 #
 # theme-specific settings
@@ -68,10 +66,10 @@ PLUGINS = ['headerid.headerid', 'tipue_search', 'sitemap']
 # Export pelican version to theme
 PELICAN_VERSION = pelican.__version__
 
-TAGLINE = 'this is where my brain dumps stuff'
+TAGLINE = "this is where my brain dumps stuff"
 
-LICENSE_NAME = 'CC BY-SA 4.0'
-LICENSE_URL = 'https://creativecommons.org/licenses/by-sa/4.0/'
+LICENSE_NAME = "CC BY-SA 4.0"
+LICENSE_URL = "https://creativecommons.org/licenses/by-sa/4.0/"
 
 MASTODON_LINK = "https://hachyderm.io/@ack"
 
@@ -84,20 +82,7 @@ LOCAL_RESOURCES = True
 # plugin: sitemap
 #
 SITEMAP = {
-    'format': 'xml',
-    'priorities': {
-        'articles': 0.5,
-        'indexes': 0.5,
-        'pages': 0.5
-    },
-    'changefreqs': {
-        'articles': 'monthly',
-        'indexes': 'daily',
-        'pages': 'monthly'
-    }
+    "format": "xml",
+    "priorities": {"articles": 0.5, "indexes": 0.5, "pages": 0.5},
+    "changefreqs": {"articles": "monthly", "indexes": "daily", "pages": "monthly"},
 }
-
-#
-# plugin: headerid
-#
-HEADERID_LINK_CHAR = '¶'
--- publishconf.py
+++ publishconf.py
@@ -1,22 +1,23 @@
 import os
 import sys
+
 sys.path.append(os.curdir)
 
 from pelicanconf import *
 
 DELETE_OUTPUT_DIRECTORY = True
 
-DOMAIN = 'https://albertodonato.net/'
+DOMAIN = "https://albertodonato.net/"
 
-SITEURL = DOMAIN + 'blog'
+SITEURL = DOMAIN + "blog"
 FEED_DOMAIN = SITEURL
 
 #
 # theme-specific settings
 #
-SEARCH_URL = 'search'
+SEARCH_URL = "search"
 
 LOCAL_RESOURCES = False
 
-FAVICON_URL = DOMAIN + 'favicon.png'
-GOOGLE_ANALYTICS = 'G-C9NP89895H'
+FAVICON_URL = DOMAIN + "favicon.png"
+GOOGLE_ANALYTICS = "G-C9NP89895H"
